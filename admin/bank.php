f97ac2ede9c84d722dbc8bc4e7b4889eabe3b64b
Dennys Matthew <me@gnztmpz.ovh>
2025-07-06 03:10:30 +0700

    bank revamp

--- admin/bank.php
+++ admin/bank.php
@@ -62,178 +62,162 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
 ?>
 <?php include 'header.php'; ?>
 
-<div class="content-wrapper">
+<!-- Body: Titel Header -->
+<div class="body-header border-bottom d-flex py-3">
+  <div class="container-xxl">
+    <div class="row align-items-center g-2">
+      <div class="col">
+        <!-- Pretitle -->
+        <h1 class="h4 mt-1">Kelola Rekening Bank</h1>
+      </div>
+      <div class="col-12 col-md-6 text-md-end">
+        <button type="button" class="btn btn-primary lift" data-bs-toggle="modal" data-bs-target="#modalTambahBank">Tambah Rekening Bank</button>
+      </div>
+    </div> <!-- Row end  -->
+  </div>
+</div>
 
-  <section class="content-header">
-    <h1>
-      Bank
-      <small>Data bank</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li class="active">Dashboard</li>
-    </ol>
-  </section>
-
-  <section class="content">
-    <div class="row">
-      <section class="col-lg-12">
-        <div class="box box-info">
-
-          <div class="box-header">
-            <h3 class="box-title">Data Akun Bank</h3>
-            <div class="btn-group pull-right">
-              <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#modalTambahBank">
-                <i class="fa fa-plus"></i> &nbsp Tambah Bank
+<!-- Body: Body -->
+<div class="body d-flex py-3">
+  <div class="container-xxl">
+    <!-- Modal Tambah Bank -->
+    <div class="modal fade" id="modalTambahBank" tabindex="-1" aria-labelledby="modalTambahBankLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <form action="bank.php" method="post">
+            <input type="hidden" name="action" value="add">
+            <div class="modal-header">
+              <h5 class="modal-title" id="modalTambahBankLabel">Tambah Bank</h5>
+              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
               </button>
             </div>
-          </div>
-          <div class="box-body">
+            <div class="modal-body">
+              <div class="form-group">
+                <label>Nama bank</label>
+                <select class="form-control" name="nama" required="required" class="" size="1">
+                  <option value="">-- Pilih Bank --</option>
+                  <optgroup label="Bank Pemerintah">
+                    <option value="Bank Mandiri">Bank Mandiri</option>
+                    <option value="Bank BNI">Bank Negara Indonesia (BNI)</option>
+                    <option value="Bank BRI">Bank Rakyat Indonesia (BRI)</option>
+                    <option value="Bank BTN">Bank Tabungan Negara (BTN)</option>
+                  </optgroup>
 
-            <!-- Modal Tambah Bank -->
-            <div class="modal fade" id="modalTambahBank" tabindex="-1" aria-labelledby="modalTambahBankLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <form action="bank.php" method="post">
-                    <input type="hidden" name="action" value="add">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="modalTambahBankLabel">Tambah Bank</h5>
-                      <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                      <div class="form-group">
-                        <label>Nama bank</label>
-                        <select class="form-control" name="nama" required="required" class="" size="1">
-                          <option value="">-- Pilih Bank --</option>
-                          <optgroup label="Bank Pemerintah">
-                            <option value="Bank Mandiri">Bank Mandiri</option>
-                            <option value="Bank BNI">Bank Negara Indonesia (BNI)</option>
-                            <option value="Bank BRI">Bank Rakyat Indonesia (BRI)</option>
-                            <option value="Bank BTN">Bank Tabungan Negara (BTN)</option>
-                          </optgroup>
+                  <optgroup label="Bank Pembangunan Daerah">
+                    <option value="Bank BPD Aceh">Bank Aceh</option>
+                    <option value="Bank BPD Bali">Bank Bali</option>
+                    <option value="Bank Bank DKI">Bank DKI</option>
+                    <option value="Bank BJB">Bank Jabar Banten (BJB)</option>
+                    <option value="Bank Jateng">Bank Jateng</option>
+                    <option value="Bank Jatim">Bank Jatim</option>
+                    <option value="Bank Kalsel">Bank Kalimantan Selatan</option>
+                    <option value="Bank Kaltim">Bank Kalimantan Timur</option>
+                    <option value="Bank Lampung">Bank Lampung</option>
+                    <option value="Bank Maluku">Bank Maluku</option>
+                    <option value="Bank Nagari">Bank Nagari</option>
+                    <option value="Bank NTT">Bank NTT</option>
+                    <option value="Bank Papua">Bank Papua</option>
+                    <option value="Bank Riau">Bank Riau Kepri</option>
+                    <option value="Bank Sultra">Bank Sulawesi Tenggara</option>
+                    <option value="Bank Sumsel">Bank Sumsel Babel</option>
+                    <option value="Bank Sumut">Bank Sumut</option>
+                  </optgroup>
 
-                          <optgroup label="Bank Pembangunan Daerah">
-                            <option value="Bank BPD Aceh">Bank Aceh</option>
-                            <option value="Bank BPD Bali">Bank Bali</option>
-                            <option value="Bank Bank DKI">Bank DKI</option>
-                            <option value="Bank BJB">Bank Jabar Banten (BJB)</option>
-                            <option value="Bank Jateng">Bank Jateng</option>
-                            <option value="Bank Jatim">Bank Jatim</option>
-                            <option value="Bank Kalsel">Bank Kalimantan Selatan</option>
-                            <option value="Bank Kaltim">Bank Kalimantan Timur</option>
-                            <option value="Bank Lampung">Bank Lampung</option>
-                            <option value="Bank Maluku">Bank Maluku</option>
-                            <option value="Bank Nagari">Bank Nagari</option>
-                            <option value="Bank NTT">Bank NTT</option>
-                            <option value="Bank Papua">Bank Papua</option>
-                            <option value="Bank Riau">Bank Riau Kepri</option>
-                            <option value="Bank Sultra">Bank Sulawesi Tenggara</option>
-                            <option value="Bank Sumsel">Bank Sumsel Babel</option>
-                            <option value="Bank Sumut">Bank Sumut</option>
-                          </optgroup>
+                  <optgroup label="Bank Swasta Nasional">
+                    <option value="Bank BCA">Bank Central Asia (BCA)</option>
+                    <option value="Bank CIMB Niaga">Bank CIMB Niaga</option>
+                    <option value="Bank Danamon">Bank Danamon</option>
+                    <option value="Bank Maybank">Bank Maybank Indonesia</option>
+                    <option value="Bank Mega">Bank Mega</option>
+                    <option value="Bank OCBC NISP">Bank OCBC NISP</option>
+                    <option value="Bank Panin">Bank Panin</option>
+                    <option value="Bank Permata">Bank Permata</option>
+                  </optgroup>
 
-                          <optgroup label="Bank Swasta Nasional">
-                            <option value="Bank BCA">Bank Central Asia (BCA)</option>
-                            <option value="Bank CIMB Niaga">Bank CIMB Niaga</option>
-                            <option value="Bank Danamon">Bank Danamon</option>
-                            <option value="Bank Maybank">Bank Maybank Indonesia</option>
-                            <option value="Bank Mega">Bank Mega</option>
-                            <option value="Bank OCBC NISP">Bank OCBC NISP</option>
-                            <option value="Bank Panin">Bank Panin</option>
-                            <option value="Bank Permata">Bank Permata</option>
-                          </optgroup>
+                  <optgroup label="Bank Syariah">
+                    <option value="Bank BSI">Bank Syariah Indonesia (BSI)</option>
+                    <option value="Bank BCA Syariah">BCA Syariah</option>
+                    <option value="Bank BNI Syariah">BNI Syariah</option>
+                    <option value="Bank BRI Syariah">BRI Syariah</option>
+                    <option value="Bank Muamalat">Bank Muamalat</option>
+                    <option value="Bank Panin Dubai Syariah">Bank Panin Dubai Syariah</option>
+                  </optgroup>
 
-                          <optgroup label="Bank Syariah">
-                            <option value="Bank BSI">Bank Syariah Indonesia (BSI)</option>
-                            <option value="Bank BCA Syariah">BCA Syariah</option>
-                            <option value="Bank BNI Syariah">BNI Syariah</option>
-                            <option value="Bank BRI Syariah">BRI Syariah</option>
-                            <option value="Bank Muamalat">Bank Muamalat</option>
-                            <option value="Bank Panin Dubai Syariah">Bank Panin Dubai Syariah</option>
-                          </optgroup>
-
-                          <optgroup label="Bank Asing & Campuran">
-                            <option value="Bank Citibank">Citibank</option>
-                            <option value="Bank HSBC">HSBC Indonesia</option>
-                            <option value="Bank Standard Chartered">Standard Chartered</option>
-                            <option value="Bank ANZ">ANZ Indonesia</option>
-                            <option value="Bank DBS">DBS Indonesia</option>
-                          </optgroup>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label>Nama Pemilik Rekening</label>
-                        <input type="text" name="pemilik" class="form-control" placeholder="Nama pemiliki rekening bank ..">
-                      </div>
-                      <div class="form-group">
-                        <label>Nomor Rekening</label>
-                        <input type="text" name="nomor" class="form-control" placeholder="Nomor rekening bank ..">
-                      </div>
-                      <div class="form-group">
-                        <label>Saldo Awal</label>
-                        <input type="number" name="saldo" required="required" class="form-control" placeholder="Saldo bank ..">
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                      <button type="submit" class="btn btn-primary">Simpan</button>
-                    </div>
-                  </form>
-                </div>
+                  <optgroup label="Bank Asing & Campuran">
+                    <option value="Bank Citibank">Citibank</option>
+                    <option value="Bank HSBC">HSBC Indonesia</option>
+                    <option value="Bank Standard Chartered">Standard Chartered</option>
+                    <option value="Bank ANZ">ANZ Indonesia</option>
+                    <option value="Bank DBS">DBS Indonesia</option>
+                  </optgroup>
+                </select>
+              </div>
+              <div class="form-group">
+                <label>Nama Pemilik Rekening</label>
+                <input type="text" name="pemilik" class="form-control" placeholder="Nama pemiliki rekening bank ..">
+              </div>
+              <div class="form-group">
+                <label>Nomor Rekening</label>
+                <input type="text" name="nomor" class="form-control" placeholder="Nomor rekening bank ..">
+              </div>
+              <div class="form-group">
+                <label>Saldo Awal</label>
+                <input type="number" name="saldo" required="required" class="form-control" placeholder="Saldo bank ..">
               </div>
             </div>
-
-            <div class="table-responsive">
-              <table class="table table-bordered table-striped" id="table-datatable">
-                <thead>
-                  <tr>
-                    <th width="1%">NO</th>
-                    <th>NAMA BANK</th>
-                    <th>PEMILIK REKENING</th>
-                    <th>NOMOR REKENING</th>
-                    <th>SALDO</th>
-                    <th width="10%">OPSI</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  $no = 1;
-                  $data = mysqli_query($koneksi, "SELECT * FROM bank");
-                  while ($d = mysqli_fetch_array($data)) {
-                  ?>
-                    <tr>
-                      <td><?php echo $no++; ?></td>
-                      <td><?php echo $d['bank_nama']; ?></td>
-                      <td><?php echo $d['bank_pemilik']; ?></td>
-                      <td><?php echo $d['bank_nomor']; ?></td>
-                      <td><?php echo "Rp. " . number_format($d['bank_saldo']) . " ,-"; ?></td>
-                      <td>
-                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#edit_bank_<?php echo $d['bank_id'] ?>">
-                          <i class="fa fa-cog"></i>
-                        </button>
-
-                        <?php if ($d['bank_id'] != 1) { ?>
-                          <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus_bank_<?php echo $d['bank_id'] ?>">
-                            <i class="fa fa-trash"></i>
-                          </button>
-                        <?php } ?>
-                      </td>
-                    </tr>
-                  <?php
-                  }
-                  ?>
-                </tbody>
-              </table>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+              <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
-          </div>
-
+          </form>
         </div>
-      </section>
+      </div>
     </div>
-  </section>
 
+    <div class="table-responsive">
+      <table class="table table-bordered table-striped" id="table-datatable">
+        <thead>
+          <tr>
+            <th width="1%">NO</th>
+            <th>NAMA BANK</th>
+            <th>PEMILIK REKENING</th>
+            <th>NOMOR REKENING</th>
+            <th>SALDO</th>
+            <th width="10%">OPSI</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $no = 1;
+          $data = mysqli_query($koneksi, "SELECT * FROM bank");
+          while ($d = mysqli_fetch_array($data)) {
+          ?>
+            <tr>
+              <td><?php echo $no++; ?></td>
+              <td><?php echo $d['bank_nama']; ?></td>
+              <td><?php echo $d['bank_pemilik']; ?></td>
+              <td><?php echo $d['bank_nomor']; ?></td>
+              <td><?php echo "Rp. " . number_format($d['bank_saldo']) . " ,-"; ?></td>
+              <td>
+                <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#edit_bank_<?php echo $d['bank_id'] ?>">
+                  <i class="fa fa-cog"></i>
+                </button>
+
+                <?php if ($d['bank_id'] != 1) { ?>
+                  <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus_bank_<?php echo $d['bank_id'] ?>">
+                    <i class="fa fa-trash"></i>
+                  </button>
+                <?php } ?>
+              </td>
+            </tr>
+          <?php
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 
 <?php
