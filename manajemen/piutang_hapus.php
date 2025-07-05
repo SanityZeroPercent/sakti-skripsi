7cabb38ad61edb31bef32492d0be4d85eba57e7a
Dennys Matthew <me@gnztmpz.ovh>
2025-07-06 03:24:53 +0700

    remove unused files

--- manajemen/piutang_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if ($_SESSION['status'] != "manajemen_logedin") {
-	header("location:../index.php?alert=belum_login");
-}
-include '../koneksi.php';
-$id  = $_GET['id'];
-
-mysqli_query($koneksi, "delete from piutang where piutang_id='$id'");
-header("location:piutang.php");
